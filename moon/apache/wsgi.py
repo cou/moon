7aac7dc8f97ee49471c4353b460d78e237f7c7e6
cou <g.shokora@gmail.com>
2014-07-23 10:15:13 +0900

    wsgi

--- moon/apache/wsgi.py
+++ moon/apache/wsgi.py
@@ -1,5 +1,6 @@
 from pyramid.paster import get_app, setup_logging
 import os
+os.environ['PYTHON_EGG_CACHE'] = '/tmp/.python-eggs'
 __here__ = os.path.dirname(os.path.abspath(__file__))
 __parent__ = os.path.dirname(__here__)
 ini_path = '%s/development.ini'%(__parent__)
